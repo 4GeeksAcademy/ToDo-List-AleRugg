7f2dc78fdd5444085348e611e7d5e2c852543411
Alerugg <134484194+Alerugg@users.noreply.github.com>
2024-08-30 17:36:20 +0000

    eliminado codigo que estaba de mas y agregado comentarios identificando las funciones

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const input = document.querySelector("#input");
 button.addEventListener("click", () => {
   const newLi = document.createElement("li");
 
-  newLi.innerHTML = `${input.value} <i class="bi bi-trash3-fill"></i>`;
+  newLi.innerHTML = `${input.value} <i class="bi bi-trash3-fill"></i>`; // < ----- esta funcion crea un li nuevo al darle al boton
   newLi.classList = "liRow";
 
   ul.appendChild(newLi);
@@ -20,19 +20,18 @@ button.addEventListener("click", () => {
 input.addEventListener("keyup", event => {
   if (event.key === "Enter" && input.value !== "") {
     const newLi = document.createElement("li");
-    newLi.innerHTML = `${input.value} <i class="bi bi-trash3-fill"></i>`;
+    newLi.innerHTML = `${input.value} <i class="bi bi-trash3-fill"></i>`; // < ----- esta funcion crea un li nuevo al darle Enter
     newLi.classList = "liRow";
 
     ul.appendChild(newLi);
 
-    input.value = "";
+    input.value = ""; // <----- esta opcion hace que al agregar el imput que le pongas, se restablezca el imput para que este en blanco y puedas escribir otra cosa
   }
 });
 
-const icon = document.querySelector(".bi-trash3-fill");
-
 ul.addEventListener("click", event => {
   if (event.target.classList.contains("bi-trash3-fill")) {
+    // < ----- esta funcion elimina de la lista el li que desees borrar al darle al icono
   }
   const li = event.target.closest("li");
   if (li) {
